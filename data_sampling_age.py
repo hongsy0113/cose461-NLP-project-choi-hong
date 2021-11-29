152946ac7adc20e802c490a10f4b20fcdbfa9cfb
hongsy0113 <87005840+hongsy0113@users.noreply.github.com>
2021-11-29 13:12:26 +0900

    Update and rename data_sampling2.py to data_sampling_age.py
    제출전

--- data_sampling_age.py
+++ data_sampling_age.py
@@ -13,8 +13,6 @@ from soynlp.normalizer import *
 from konlpy.tag import Okt
 import re
 
-file_list = ['living.json']
-
 # json file을 읽어오고 dataframe으로 변환하는 함수
 def json2df (file):
   with open(file, 'r', encoding='UTF8') as f:
@@ -51,12 +49,13 @@ def json2df (file):
   return df
 
 # input dataframe을 우리의 형식에 맞게 변형(i.e. 화자가 보낸 메시지로 묶어줌)
+
 def clean_df_age(df):
   age12 = ['10대', '20대']
   age34 = ['30대', '40대']
   age567 = ['50대', '60대', '70대']
-  
-  df['P_age'] = [0 if age in age12 else (1 if age in age34 else(2 if age in age567 else np.nan)) for age in df['P_age']]
+
+  df['P_age'] = [0 if age in age12 else (1 if age in age34 else (2 if age in age567 else np.nan)) for age in df['P_age']]
   df = df.set_index(['D_id','P_id'] )
   df = df.sort_index()
   df['index'] = df.index.to_numpy()
@@ -69,6 +68,7 @@ def clean_df_age(df):
 
   return df
 
+
 # input dataframe을 주어진 비율로 sample
 def sample_df(df, ratio):
   n = int(len(df) * ratio)
@@ -76,27 +76,34 @@ def sample_df(df, ratio):
 
   return result
 
-# gender 비율 맞춰주는 함수
+# data label 비율 맞춰주는 함수
 # sample을 먼저 할까, gender 비율을 먼저 맞출까 하다가 큰 상관 없을 거 같아서 sampling 먼저 하고 gender 비율 맞춤
+
+
 def fix_age_bias(df):
   df_12 = df[df['P_age']==0]
   df_34 = df[df['P_age']==1]
   df_567 = df[df['P_age']==2]
+
+
   print()
+ 
   print("10-20대 수 : {}, 30-40대 수 : {}, 50-70대 수 : {}".format(df_12['P_age'].value_counts()[0], df_34['P_age'].value_counts()[1], df_567['P_age'].value_counts()[2])) 
 
   df_12 = df_12.sample(n= df_567['P_age'].value_counts()[2], random_state=42)
   df_34 = df_34.sample(n= df_567['P_age'].value_counts()[2], random_state=42)
+
   df = pd.concat([df_12, df_34, df_567],sort=False)
-  print("----After adjusting gender bias in dataset----")
+  print("----After adjusting data imbalance in dataset----")
   print("10-20대 수 : {}, 30-40대 수 : {}, 50-70대 수 : {}".format(df_12['P_age'].value_counts()[0], df_34['P_age'].value_counts()[1], df_567['P_age'].value_counts()[2])) 
 
   del df_12
-  gc.collect()
+  gc.collect() 
   del df_34
   gc.collect()
   del df_567
   gc.collect()
+
 
   return df
 
@@ -142,11 +149,6 @@ def json2csv (jsonfile_list, ratio, output_file):
 
     return result 
 
-### 전처리까지 진행해서 다시 csv로 저장
-
-def preprocess (df):
-  pass
-
 
 
 json_list = [
@@ -162,47 +164,5 @@ json_list = [
             ]
 
 
+json2csv(json_list, 1, 'age_test.csv') 
 
-
-#json_list = ['food.json']
-#sample_and_merge (json_list, 0.
-#json2csv(json_list, 1, 'age_data_100.csv') 
-
-
-
-''''''''''''''''''''''''
-
-
-
-okt = Okt()
-
-def clean(doc) :
-
-  doc = doc.replace('#@이모티콘#', '이모티콘')
-
-  pattern = '(#@[^#]+#)'
-  doc = re.sub(pattern, '', doc)
-
-  new_doc = list()
-  doc = okt.pos(doc, norm=True)
-
-  stop_tags = ['Determiner', 'Josa'] #, 'Foreign'] # 'foreign' 은 살려보자. ((이모티콘))으로 통일
-  stop_words = ['은', '는', '이', '가', '']
-  for text, tag in doc:  
-
-    if tag in stop_tags:
-      continue
-    
-    if tag == 'Foreign':
-      text = '((이모티콘))'
-
-    text = re.sub(r'[^ㄱ-ㅣ가-힣?.!~:())\^]+', '', text)  # remove digits. ^, :, ) 는 들어가게
-    text = emoticon_normalize(text, num_repeats=2) # remove repeated emoticon. e.g) ㅋㅋㅋㅋ=>ㅋㅋ, ㅠㅠㅠㅠ=>ㅠㅠ
-    text = repeat_normalize(text, num_repeats=1) # remove repeated character
-    
-    if text in stop_words or (tag=='Verb' and len(text)<=1):
-      continue
-      
-    new_doc.append(text)
-
-  return new_doc
